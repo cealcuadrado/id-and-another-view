e5e95d1ac4be81f4ad2d68a9ea52d8d7e3405d04
CeAlCuadrado <cealcuadrado@outlook.com>
2022-03-09 15:03:13 -0300

    Creados módulos de prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'foo', loadChildren: () => import('./foo/foo.module').then(m => m.FooModule) },
+  { path: 'one', loadChildren: () => import('./one/one.module').then(m => m.OneModule) },
+  { path: 'two', loadChildren: () => import('./two/two.module').then(m => m.TwoModule) },
+  { path: '', redirectTo: '/foo', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
